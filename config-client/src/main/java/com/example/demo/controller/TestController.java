683598266c104011d888a0c2412b61e152ee4ca1
林杰 <linjieb@landicorp.com>
2018-11-27 18:41:51 +0800

    first commit

---
+++ config-client/src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by admin on 2018/11/27.
+ */
+@RestController
+public class TestController {
+
+    @Value("${foo}")
+    String foo;
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return foo;
+    }
+}
