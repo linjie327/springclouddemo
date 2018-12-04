195027518043b9ce6f99a7f37f06e8ed338a21f0
林杰 <linjieb@landicorp.com>
2018-12-04 16:55:23 +0800

    5

--- eureka-client/src/main/java/com/example/demo/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/example/demo/EurekaClientApplication.java
@@ -4,7 +4,10 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class EurekaClientApplication {
 
 	public static void main(String[] args) {
